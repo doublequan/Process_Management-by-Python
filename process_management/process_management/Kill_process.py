d976a6c04e13353e4de2848166aedcd1b31d3182
doublequan <msa_quanquan@163.com>
2015-09-14 16:00:47 +0800

    improve kill_process
    kill_process.py

--- process_management/process_management/Kill_process.py
+++ process_management/process_management/Kill_process.py
@@ -5,22 +5,39 @@ import time
 
 NORMAL = 1
 CONTAINS = 2
-START_WITH = 3
+START_WITH = 3 
 
-def killProcess(name, flag = NORMAL):
+def killProcess(list, flag = NORMAL):
     ''' kill the Process by the input @name
-    the flag CONTAINS : kill all the precesses that contain @name
+        flag   CONTAINS : kill all the precesses that contain @name
              START_WITH : kill all the precesses that start with @name
-             NORMAL : kill all the precesses that has the exactly same name with the argument @name
+                 NORMAL : kill all the precesses that has the exactly same name with the argument @name
     '''
-    WMI = win32com.client.GetObject('winmgmts:')
+    #form the sql query  
+    names = list.copy()
+    query = 'select * from Win32_Process where '
     if flag == 1:
-        query = 'select * from Win32_Process where Name LIKE "'+name+'"'   
+        while len(names) != 0:
+            query += 'Name LIKE "'+names.pop()+'"'
+            if len(names) != 0 :
+                query += ' or '   
     elif flag == 2:
-        query = 'select * from Win32_Process where Name LIKE "%'+name+'%"'
+        while len(names) != 0:
+            query += 'Name LIKE "%'+names.pop()+'%"'
+            if len(names) != 0 :
+                query += ' or ' 
     elif flag == 3:
-        query = 'select * from Win32_Process where Name LIKE "'+name+'%"'
+        while len(names) != 0:
+            query += 'Name LIKE "'+names.pop()+'%"'
+            if len(names) != 0 :
+                query += ' or ' 
+    else:
+        return False
+
+    #find all the process required by the query
+    WMI = win32com.client.GetObject('winmgmts:')
     p_list = WMI.ExecQuery(query)
+
     c = wmi.WMI()
     for p in p_list:
             # filter commandline from java processes
@@ -32,7 +49,7 @@ def killProcess(name, flag = NORMAL):
                           #TODO:
                           #write log
                           print 'Killed ' + str(process.ProcessId) + str(process.Name)
-                          process.Terminate()
+                          #process.Terminate()
                           #print process.ProcessId, process.Name
                       #for ia_p in ia_process:
                             # Terminate process
--- process_management/process_management/process_management.py
+++ process_management/process_management/process_management.py
@@ -1,8 +1,12 @@
-import Kill_process
+﻿import Kill_process
 import time
+
+
+list = {'chrome','csrss'}
+
 
 #main loop, check process list every 3 seconds
 while True:
     print time.strftime('%Y-%m-%d %X', time.localtime())
-    Kill_process.killProcess("xfplay", 3)   
+    Kill_process.killProcess(list, Kill_process.START_WITH)   
     time.sleep(3) 
